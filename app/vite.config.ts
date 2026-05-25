3ce3468f7cfbb3afc0e2c8ce7e16568c2ff24a80
Facu Echenique <facueche@gmail.com>
2026-05-25 17:52:20 -0300

    feat: implement shadcn as css library in app

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,3 +1,5 @@
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -5,10 +7,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
   return {
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
     server: {
       host: true,
       port: parseInt(env.VITE_PORT) || 5173,
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
   }
 })
